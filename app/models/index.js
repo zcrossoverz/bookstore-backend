4e96f42e49ca018e28041cc68c77561b50c2b4ed
zcrossoverz <56151228+zcrossoverz@users.noreply.github.com>
2021-11-16 10:16:21 +0700

    create model order

--- app/models/index.js
+++ app/models/index.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 const createUserModel = require("./user.model");
+const createOrderModel = require("./order.model");
 
 const db = {};
 
 db.mongoose = mongoose;
 db.User = createUserModel(mongoose);
+db.Order = createOrderModel(mongoose);
 
 module.exports = db;
---
+++ app/models/order.model.js
@@ -0,0 +1,34 @@
+module.exports = mongoose => {
+    const schema = mongoose.Schema({
+        username: {
+            type: String,
+            required: true
+        },
+        order: [
+            {
+                id: {
+                    type: String
+                },
+                name: {
+                    type: String
+                },
+                time_order: {
+                    type: Date
+                },
+                number: {
+                    type: Number
+                },
+                price: {
+                    type: Number
+                },
+                total: {
+                    type: Number
+                }
+            }
+        ]
+    },
+    {
+        timestamps: true
+    });
+    return mongoose.model("order", schema);
+}
